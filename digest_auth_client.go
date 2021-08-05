fd3fe162fbfdcc2281ad752a7438b424df02e88a
Palinda Attanayake <palinda@Palindas-MacBook-Pro.local>
2021-08-05 15:51:28 +0530

    log change
    Signed-off-by: Palinda Attanayake <palinda@Palindas-MacBook-Pro.local>

--- digest_auth_client.go
+++ digest_auth_client.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"log"
 )
 
 type DigestRequest struct {
@@ -113,7 +114,7 @@ func (dr *DigestRequest) Execute() (resp *http.Response, err error) {
 		return nil, err
 	}
 
-	Printf("Palinda %s", resp)
+	log.Printf("Palinda %s", resp)
 	if resp.StatusCode == 401 {
 		return dr.executeNewDigest(resp)
 	}
